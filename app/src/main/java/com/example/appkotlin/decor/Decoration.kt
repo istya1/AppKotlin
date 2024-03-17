af7e2d951328ed7e44caac7436ae4094e83298f4
istyatugas1 <istyaa07@gmail.com>
2024-03-17 15:49:47 +0700

    step 8 (Use destructuring)

--- app/src/main/java/com/example/appkotlin/decor/Decoration.kt
+++ app/src/main/java/com/example/appkotlin/decor/Decoration.kt
@@ -1,6 +1,6 @@
-package com.example.appkotlin.decor
-data class Decoration(val rocks: String) {
-}
+data class Decoration(val rocks: String)
+data class Decoration2(val rocks: String, val wood: String, val diver: String)
+
 fun makeDecorations() {
     val decoration1 = Decoration("granite")
     println(decoration1)
@@ -11,10 +11,19 @@ fun makeDecorations() {
     val decoration3 = Decoration("slate")
     println(decoration3)
 
-    println (decoration1.equals(decoration2))
-    println (decoration3.equals(decoration2))
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+    println(decoration1.equals(decoration2))
+    println(decoration3.equals(decoration2))
+
+    // Assign all properties to variables.
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
 }
 
-fun main(){
+fun main() {
     makeDecorations()
 }
