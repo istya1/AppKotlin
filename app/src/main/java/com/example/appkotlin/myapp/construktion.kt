ea4e4a446f776cadceac45454baaba6388ac678d
istyatugas1 <istyaa07@gmail.com>
2024-03-17 13:27:12 +0700

    step 5 (open class)

--- app/src/main/java/com/example/appkotlin/myapp/construktion.kt
+++ app/src/main/java/com/example/appkotlin/myapp/construktion.kt
@@ -1,21 +1,9 @@
 package com.example.appkotlin.myapp
 
 fun buildAquariumm() {
-    val aquarium1 = Aquariumm()
-    aquarium1.printSize()
-    // default height and length
-    val aquarium2 = Aquariumm(width = 25)
-    aquarium2.printSize()
-    // default width
-    val aquarium3 = Aquariumm(height = 35, length = 110)
-    aquarium3.printSize()
-    // everything custom
-    val aquarium4 = Aquariumm(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
     val aquarium6 = Aquariumm(numberOfFish = 29)
     aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 }
 fun main() {
     buildAquariumm()
